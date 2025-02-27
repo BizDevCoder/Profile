8553849be91f19b16d8ddcfd6e65a275aa26d54b
Rohit Kumar <rohit20021004@gmail.com>
2025-02-27 20:53:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    dots: false,
+    responsiveClass: true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:3 ,
+            nav:false,
+            loop:false
+        }
+    }
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+    var video = document.getElementById("myVideo");
+    video.controls = false;
+});
